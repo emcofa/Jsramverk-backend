a5ec9a671069d268800e38d52ee1d038dcd219d0
emcofah <emfh21@student.bth.se>
2022-10-24 12:48:32 +0200

    fixed tests

--- models/authModel.js
+++ models/authModel.js
@@ -10,7 +10,10 @@ const jwt = require('jsonwebtoken');
 let secret;
 
 if (`${process.env.NODE_ENV}` === 'test') {
-    secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2NjYwNzkwMiwiaWF0IjoxNjY2NjA3OTAyfQ.HlFgbsbxgGJFGPjx1VmAMWNI63Fa_qT24g0L76Pb_IU";
+    secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N"
+    "1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsI"
+    "mV4cCI6MTY2NjYwNzkwMiwiaWF0IjoxNjY2NjA3OTAyfQ.HlF"
+    "gbsbxgGJFGPjx1VmAMWNI63Fa_qT24g0L76Pb_IU";
 } else {
     secret = `${process.env.ACCESS_TOKEN_SECRET}`;
 }
